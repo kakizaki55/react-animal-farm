855103d6a50153c6e83400ad6c0f30a66813b0d7
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-11-29 16:15:18 -0800

    just finished my first react app

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import './Main.css'
+import farm from '../../background.png'
+
+import Animal from '../Animal/Animal'
+import animals from '../../data.js'
 
 export default function Main() {
-  return <main></main>
+  return (
+    <main style={{ backgroundImage: `url(${farm})` }}>
+      {animals.map((animal) => {
+        return (
+          <Animal
+            key={animal.name}
+            type={animal.type}
+            name={animal.name}
+            says={animal.says}
+            top={animal.top}
+            left={animal.left}
+          />
+        )
+      })}
+    </main>
+  )
 }
