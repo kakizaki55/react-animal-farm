8f382d696d9af81e5a31f2c61b21fc525430aa1b
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-03 10:58:34 -0800

    destructed my code

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,16 +9,7 @@ export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${farm})` }}>
       {animals.map((animal) => {
-        return (
-          <Animal
-            key={animal.name}
-            type={animal.type}
-            name={animal.name}
-            says={animal.says}
-            top={animal.top}
-            left={animal.left}
-          />
-        )
+        return <Animal key={animal.name} {...animal} />
       })}
     </main>
   )
